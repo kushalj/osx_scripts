c4c994515024bb40c991613a7e916193cdbc3c3d
Kushal Joshi <kushal@joshi.com>
2014-03-22 23:41:29 +0000

    git remote comment and bashrc_custom added

--- git-remote-to-ssh.sh
+++ git-remote-to-ssh.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # Usage: {remote name} {username} {repo name}
+
+# e.g.
+# source git-remote-to-ssh.sh origin kushalj osx_scripts
+
 git remote -v
 git remote rm $1
 git remote add $1 git@github.com:$2/$3
